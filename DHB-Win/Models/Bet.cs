a813e4038c17b4baf7ee7453f2fa7388ddd28d75
simonstenzel <84180936+simonstenzel@users.noreply.github.com>
2022-05-24 00:24:29 +0200

    Bet.cs restored

--- DHB-Win/Models/Bet.cs
+++ DHB-Win/Models/Bet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DHB_Win.Models
 {
@@ -11,14 +12,38 @@ namespace DHB_Win.Models
             Placements = new HashSet<Placement>();
         }
 
+
+        [Display(Name = "Bet ID")]
         public int BetId { get; set; }
+
+        [Display(Name = "User")]
+        [Required(ErrorMessage = "User ist notwendig")]
         public int? UidFk2 { get; set; }
+
+        [Display(Name = "Titel")]
+        [Required(ErrorMessage = "Titel ist notwendig")]
         public string? Title { get; set; }
+
+        [Display(Name = "Erfahrungspunkte")]
+        [Required(ErrorMessage = "Erfahrungspunkte sind notwendig")]
+        [Range(0, 10,
+        ErrorMessage = "Erfahrungspunkte dürfen nicht über 10 sein")]
         public int? ExpPoints { get; set; }
+
+        [Display(Name = "Belohnung")]
+        [Required(ErrorMessage = "Belohnung ist notwendig")]
+        [Range(0, 100,
+        ErrorMessage = "Beohnung darf nicht über 100 sein")]
         public int? Reward { get; set; }
+
+        [Display(Name = "Beschreibung")]
+        [Required(ErrorMessage = "Beschreibung ist notwendig")]
         public string? Description { get; set; }
+
+        [Display(Name = "Datum")]
         public DateTime? CreationDate { get; set; }
 
+        [Display(Name = "User")]
         public virtual User? UidFk2Navigation { get; set; }
         public virtual ICollection<BetOption> BetOptions { get; set; }
         public virtual ICollection<Placement> Placements { get; set; }
