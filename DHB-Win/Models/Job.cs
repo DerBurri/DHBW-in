4b0ff5d418cdce0ebe6c428850ddcd0600171218
simonstenzel <84180936+simonstenzel@users.noreply.github.com>
2022-05-24 00:36:11 +0200

    DAta annotation in Auftfräge

--- DHB-Win/Models/Job.cs
+++ DHB-Win/Models/Job.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DHB_Win.Models
 {
@@ -8,17 +9,46 @@ namespace DHB_Win.Models
     /// </summary>
     public partial class Job
     {
+        [Display(Name = "Job ID")]
         public int JobId { get; set; }
+
+        [Display(Name = "Anbieter ID")]
         public int? ProviderId { get; set; }
+
+        [Display(Name = "Arbeiter ID")]
         public int? WorkerId { get; set; }
+
+        [Display(Name = "Titel")]
+        [Required(ErrorMessage = "Titel ist notwendig")]
         public string? Title { get; set; }
+
+        [Display(Name = "Beschreibung")]
+        [Required(ErrorMessage = "Beschreibung ist notwendig")]
         public string? Description { get; set; }
+
+        [Display(Name = "Belohnung")]
+        [Required(ErrorMessage = "Belohnung ist notwendig")]
+        [Range(0, 100,
+        ErrorMessage = "Beohnung darf nicht über 100 sein")]
         public int? Reward { get; set; }
+
+        [Display(Name = "Erfahrungspunkte")]
+        [Required(ErrorMessage = "Erfahrungspunkte sind notwendig")]
+        [Range(0, 10,
+        ErrorMessage = "Erfahrungspunkte dürfen nicht über 10 sein")]
         public int? ExpPoints { get; set; }
+
+        [Display(Name = "Erstelldatum")]
+        [Required(ErrorMessage = "Erstelldatum ist notwendig")]
         public DateTime? CreationDate { get; set; }
+
+        [Display(Name = "Enddatum")]
         public DateTime? FinishDate { get; set; }
 
+        [Display(Name = "Anbieter")]
         public virtual User? Provider { get; set; }
+
+        [Display(Name = "Arbeiter")]
         public virtual User? Worker { get; set; }
     }
 }
