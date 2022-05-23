e1fc6aa9d1efa7d3961934ec61943adb2fadecd2
Maximilian Burr <maximilian.burr@outlook.de>
2022-05-23 23:58:07 +0200

    Added new Controller

---
+++ DHB-Win/Controllers/AchievedAchievementsController.cs
@@ -0,0 +1,181 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DHB_Win.Models;
+
+namespace DHB_Win.Controllers
+{
+    public class AchievedAchievementsController : Controller
+    {
+        private readonly dhbwinContext _context;
+
+        public AchievedAchievementsController(dhbwinContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AchievedAchievements
+        public async Task<IActionResult> Index()
+        {
+            var dhbwinContext = _context.AchievedAchievements.Include(a => a.AchIdFkNavigation)
+                .Include(a => a.UidFkNavigation);
+            return View(await dhbwinContext.ToListAsync());
+        }
+
+        // GET: AchievedAchievements/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.AchievedAchievements == null)
+            {
+                return NotFound();
+            }
+
+            var achievedAchievement = await _context.AchievedAchievements
+                .Include(a => a.AchIdFkNavigation)
+                .Include(a => a.UidFkNavigation)
+                .FirstOrDefaultAsync(m => m.Aaid == id);
+            if (achievedAchievement == null)
+            {
+                return NotFound();
+            }
+
+            return View(achievedAchievement);
+        }
+
+        // GET: AchievedAchievements/Create
+        public IActionResult Create()
+        {
+            ViewData["AchIdFk"] = new SelectList(_context.Achievements, "AchId", "AchId");
+            ViewData["UidFk"] = new SelectList(_context.Users, "Uid", "Uid");
+            return View();
+        }
+
+        // POST: AchievedAchievements/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+            [Bind("UidFk,AchIdFk,CreationDate,Aaid")] AchievedAchievement achievedAchievement)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(achievedAchievement);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["AchIdFk"] = new SelectList(_context.Achievements, "AchId", "AchId", achievedAchievement.AchIdFk);
+            ViewData["UidFk"] = new SelectList(_context.Users, "Uid", "Uid", achievedAchievement.UidFk);
+            return View(achievedAchievement);
+        }
+
+        // GET: AchievedAchievements/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.AchievedAchievements == null)
+            {
+                return NotFound();
+            }
+
+            var achievedAchievement = await _context.AchievedAchievements.FindAsync(id);
+            if (achievedAchievement == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["AchIdFk"] = new SelectList(_context.Achievements, "AchId", "AchId", achievedAchievement.AchIdFk);
+            ViewData["UidFk"] = new SelectList(_context.Users, "Uid", "Uid", achievedAchievement.UidFk);
+            return View(achievedAchievement);
+        }
+
+        // POST: AchievedAchievements/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,
+            [Bind("UidFk,AchIdFk,CreationDate,Aaid")] AchievedAchievement achievedAchievement)
+        {
+            if (id != achievedAchievement.Aaid)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(achievedAchievement);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AchievedAchievementExists(achievedAchievement.Aaid))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["AchIdFk"] = new SelectList(_context.Achievements, "AchId", "AchId", achievedAchievement.AchIdFk);
+            ViewData["UidFk"] = new SelectList(_context.Users, "Uid", "Uid", achievedAchievement.UidFk);
+            return View(achievedAchievement);
+        }
+
+        // GET: AchievedAchievements/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.AchievedAchievements == null)
+            {
+                return NotFound();
+            }
+
+            var achievedAchievement = await _context.AchievedAchievements
+                .Include(a => a.AchIdFkNavigation)
+                .Include(a => a.UidFkNavigation)
+                .FirstOrDefaultAsync(m => m.Aaid == id);
+            if (achievedAchievement == null)
+            {
+                return NotFound();
+            }
+
+            return View(achievedAchievement);
+        }
+
+        // POST: AchievedAchievements/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.AchievedAchievements == null)
+            {
+                return Problem("Entity set 'dhbwinContext.AchievedAchievements'  is null.");
+            }
+
+            var achievedAchievement = await _context.AchievedAchievements.FindAsync(id);
+            if (achievedAchievement != null)
+            {
+                _context.AchievedAchievements.Remove(achievedAchievement);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AchievedAchievementExists(int id)
+        {
+            return (_context.AchievedAchievements?.Any(e => e.Aaid == id)).GetValueOrDefault();
+        }
+    }
+}
