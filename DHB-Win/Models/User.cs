b06c2b4a52ea560659f5faf3d9538db4a2e129e7
simonstenzel <84180936+simonstenzel@users.noreply.github.com>
2022-05-24 02:21:34 +0200

    User Interface

--- DHB-Win/Models/User.cs
+++ DHB-Win/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DHB_Win.Models
 {
@@ -14,16 +15,43 @@ namespace DHB_Win.Models
             Placements = new HashSet<Placement>();
         }
 
+        [Display(Name = "User ID")]
         public int Uid { get; set; }
+
+        [Display(Name = "Vorname")]
+        [Required(ErrorMessage = "Vorname ist notwendig")]
         public string? Firstname { get; set; }
+
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Name ist notwendig")]
         public string? Name { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email ist notwendig")]
         public string? EMail { get; set; }
+
+        [Display(Name = "Strasse")]
+        [Required(ErrorMessage = "Strasse ist notwendig")]
         public string? Street { get; set; }
+
+        [Display(Name = "Postleitzahl")]
+        [Required(ErrorMessage = "Postleitzahl ist notwendig")]
         public int? PlzFk { get; set; }
+
+        [Display(Name = "User")]
+        [Required(ErrorMessage = "User ist notwendig")]
         public int? ExpPoints { get; set; }
+
+        [Display(Name = "Passwort")]
+        [Required(ErrorMessage = "Passwort ist notwendig")]
         public string? PasswordHash { get; set; }
+
+        [Display(Name = "Kontostand")]
         public int? Walletbalance { get; set; }
+
+        [Display(Name = "Profilbild")]
         public int? Profilepicture { get; set; }
+
 
         public virtual Plz? PlzFkNavigation { get; set; }
         public virtual ICollection<AchievedAchievement> AchievedAchievements { get; set; }
